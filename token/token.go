a72346da931da3527615abc7ccaa0eb4f6d23bd4
s-li1 <stevenli8892@hotmail.com.au>
2023-06-10 15:24:07 +1000

    add basic if/else control block, return keyword and boolean

--- token/token.go
+++ token/token.go
@@ -33,11 +33,21 @@ const (
   
   FUNCTION = "FUNCTION"
   LET = "LET"
+  TRUE = "TRUE"
+  FALSE = "FALSE"
+  IF = "IF"
+  ELSE = "ELSE"
+  RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
   "fn": FUNCTION,
   "let": LET,
+  "true": TRUE,
+  "false": FALSE,
+  "return": RETURN,
+  "if": IF,
+  "else": ELSE,
 }
 
 func LookupIdent(ident string) TokenType {
